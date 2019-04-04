02018e72b43f315cfe119404bce87d26156e5b6e
Marc Worrell <marc@worrell.nl>
2019-04-04 17:09:41 +0200

    Rename 'socket' to 'smtp_socket' (#169)
    This fixes compatibility with OTP-22 which introduces a socket module.

--- src/smtp_socket.erl
+++ src/smtp_socket.erl
@@ -20,7 +20,7 @@
 %%% WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 %% @doc Facilitates transparent gen_tcp/ssl socket handling
--module(socket).
+-module(smtp_socket).
 
 
 -define(TCP_LISTEN_OPTIONS,[  {active, false},
@@ -329,9 +329,9 @@ set_sockopt(ListenObject, ClientSocket) ->
 		{ok, Opts} ->
 			case prim_inet:setopts(ClientSocket, Opts) of
 				ok -> ok;
-				Error -> socket:close(ClientSocket), Error
+				Error -> smtp_socket:close(ClientSocket), Error
 			end;
-		Error -> socket:close(ClientSocket), Error
+		Error -> smtp_socket:close(ClientSocket), Error
 	end.
 
 -ifdef(TEST).
@@ -628,7 +628,7 @@ ssl_upgrade_test_() ->
 			spawn(fun() ->
 			      	{ok, ListenSocket} = listen(tcp, ?TEST_PORT),
 			      	{ok, ServerSocket} = accept(ListenSocket),
-							{ok, NewServerSocket} = socket:to_ssl_server(ServerSocket, [{keyfile, "test/fixtures/server.key"}, {certfile, "test/fixtures/server.crt"}]),
+							{ok, NewServerSocket} = smtp_socket:to_ssl_server(ServerSocket, [{keyfile, "test/fixtures/server.key"}, {certfile, "test/fixtures/server.crt"}]),
 			      	Self ! NewServerSocket
 			      end),
 			{ok, ClientSocket} = connect(tcp, "localhost", ?TEST_PORT),
