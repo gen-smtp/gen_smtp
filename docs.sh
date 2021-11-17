c90055b986803d322f490519a384125a8d82db61
cw789 <789@webuhu.at>
2021-11-17 14:47:44 +0100

    Use fixed ex_doc version
    Additionally removed ability to manually set source-ref.

--- docs.sh
+++ docs.sh
@@ -6,11 +6,10 @@ set -e
 
 # Usage:
 # sh ./docs.sh
-# sh ./docs.sh <source-ref>
-# Without any parameter source link inference (source-ref)
-# will be set to the app version. Means a Git tag for the version must exist!
 
-SOURCE_REF=$1
+# Note:
+# Source link inference (source-ref) will be set to the app version.
+# Means a Git tag for the version must exist!
 
 app_name="gen_smtp"
 
@@ -22,17 +21,12 @@ EOF
 )
 app_vsn=$(erl -noshell -eval "$escript_get_app_vsn")
 
-[[ -z $SOURCE_REF ]] \
-	&& source_ref=$app_vsn \
-	|| source_ref=$SOURCE_REF
-
 rebar3 compile
 rebar3 edoc
 
 ~/.mix/escripts/ex_doc $app_name $app_vsn "_build/default/lib/$app_name/ebin" \
 	--config docs.config \
-	--source-ref $source_ref \
+	--source-ref $app_vsn \
 	--paths "_build/default/lib/*/ebin"
 
 echo "Generated docs v${app_vsn} (by $(~/.mix/escripts/ex_doc --version))"
-echo "Source link inference: ${source_ref}"
